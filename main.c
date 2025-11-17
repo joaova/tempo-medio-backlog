b698880a889ba85b2247487b6caa3d029fbe2368
Bianca Celi dos Santos <biancacelisantos@gmail.com>
2025-11-17 19:50:12 -0300

    mudanças para leitura do arquivo de nomes - adicionando variaveis

--- main.c
+++ main.c
@@ -19,15 +19,17 @@ int main(int argc, char *argv[]) //argc conta o n�mero de par�metros e argv 
 
     clock_t start, end; //para contar o tempo decorrido
 
-    FILE * entrada;
+    FILE * entrada_dados;
+    FILE * entrada_usuario;
     FILE * saida;
 
-    char *palavra, linha[1000]; // linhas a serem lidas do arquivo
+    char *titulo_jogo, linha[1000]; // linhas a serem lidas do arquivo
+    float horas;
     char separador = ',';
 
     //comentario
 
-    if (argc!=3)  //o numero de parametros esperado � 3: nome do programa (argv[0]), nome do arq de entrada(argv[1]), nome do arq de saida(argv[2])
+    if (argc!=4)  //o numero de parametros esperado � 3: nome do programa (argv[0]), nome do arq de entrada(argv[1]), nome do arq de saida(argv[2])
     {
         printf ("N�mero incorreto de par�metros.\n Para chamar o programa digite: exemplo <arq_entrada> <arq_saida>\n");
         return 1;
@@ -35,44 +37,46 @@ int main(int argc, char *argv[]) //argc conta o n�mero de par�metros e argv 
     else
     {
 
-        entrada = fopen (argv[1], "r"); // abre o arquivo para leitura -- argv[1] � o primeiro par�metro ap�s o nome do arquivo.
-        if (entrada == NULL) //se n�o conseguiu abrir o arquivo
+        entrada_dados = fopen (argv[1], "r"); // abre o arquivo para leitura -- argv[1] � o primeiro par�metro ap�s o nome do arquivo.
+        entrada_usuario = fopen (argv[2], "r");
+
+        if (entrada_dados == NULL || entrada_usuario == NULL) //se n�o conseguiu abrir o arquivo
         {
+            if (entrada_dados == NULL)
             printf ("Erro ao abrir o arquivo %s",argv[1]);
+            else
+            printf ("Erro ao abrir o arquivo %s",argv[2]);
             return 1;
         }
+
         else // arquivo de entrada OK
 
         {
-            saida = fopen (argv[2], "w"); // abre o arquivo para saida -- argv[2] � o segundo par�metro ap�s o nome do arquivo.
+            saida = fopen (argv[3], "w"); // abre o arquivo para saida -- argv[2] � o segundo par�metro ap�s o nome do arquivo.
 
             start = clock(); //inicia a contagem do tempo
 
 
             //percorre todo o arquivo lendo linha por linha
-            while (fgets(linha,1000,entrada))
+            while (fgets(linha,1000,entrada_dados))
             {
-                palavra = strtok (linha, separador); //considera qquer caractere n�o alfab�tico como separador
-
-                while (palavra != NULL)
-                {
-                    fprintf(saida,"%s ", strlwr(palavra)); //strlwr � a fun��o que converte palavras para min�sculo
-                    palavra = strtok (NULL, separador);
-                }
+                titulo_jogo = strtok (linha, separador); //considera qquer caractere n�o alfab�tico como separador
+                horas = atof(strtok (NULL, separador));
             }
 
-            printf("\nArquivo %s gerado com sucesso.\n",argv[2]);
+            printf("\nArquivo %s gerado com sucesso.\n",argv[3]);
 
             end = clock(); // finaliza contagem do tempo
             float miliseconds = (float)(end - start) / CLOCKS_PER_SEC * 1000; //calcula o tempo decorrido
             printf("Tempo: %.5f ms\n",miliseconds);
         }
-        fclose (entrada); //fecha os arquivos
+        fclose (entrada_dados); //fecha os arquivos
+        fclose (entrada_usuario); 
         fclose (saida);
         return 0;
     }
 }
 
-//to testando o git - bianca
-//ainda não apareceu o M
-//agora foi
+//mudei algumas coisas como tinhamos falado naquele dia. fica a vontade pra testar se quiser. 
+//ainda não faz nada com o arquivo que só vai ter jogos
+//ja ta mais certo lidando com o outro e ja existe as variaveis, inicialização e etc do arquivo q nn tinha
